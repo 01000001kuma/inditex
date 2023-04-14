b528772ae7135d79d05fc3cdfc19e7dfc40acf67
01000001kuma <a_illera@yahoo.com>
2023-04-14 17:25:53 +0200

    wip api calls

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { obtenerTodo } from './funciones';
 
 function App() {
 
-  const [podcast, setPodcast] = useState([]);
+  const [podcast, setPodcast] = useState({});
 
   useEffect(() => {
     obtenerTodo(setPodcast);
@@ -14,9 +14,27 @@ function App() {
 
 
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="contenedor ">
+      <div>
+        <h1>Podcast</h1>
+        <div className="row">
+          {podcast.entry && podcast.entry.map((podcast) => (
+            <div className="col-4" key={podcast.id.attributes['im:id']}>
+              <div className="card">
+                <img src={podcast['im:image'][2].label} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{podcast['im:name'].label}</h5>
+                  <p className="card-text">{podcast['im:artist'].label}</p>
+
+                  </div>
+                  </div>
+                  </div>
+          ))}
+                
+            
+      </div>
     </div>
+  </div>
   );
 }
 
--- src/funciones.js
+++ src/funciones.js
@@ -2,7 +2,5 @@ export const obtenerTodo = async( setPodcast ) => {
     const peticion = await fetch('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')
     const {feed} = await peticion.json();
     setPodcast(feed);
-    
-    console.log(feed)
 
 };
