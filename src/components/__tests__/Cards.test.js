45002978216614b9a6bccc64f2cc0cfcfabff0de
01000001kuma <a_illera@yahoo.com>
2023-04-19 12:53:13 +0200

    tests for cards component

--- src/components/__tests__/Cards.test.js
+++ src/components/__tests__/Cards.test.js
@@ -1,3 +1,21 @@
 import React from "react";
 import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import Cards from "../components/Cards";
+
+describe('Cards coomponent', () => {
+  test('renders Cards component', () => {
+    render(<Cards />);
+    const title= screen.getByText(/Buscar podcast/i);
+    expect(title).toBeInTheDocument();
+  });
+
+  test('filters podcasts based on search term', async () => {
+    render(<Cards />);
+    const searchImput = screen.getByPlaceholderText(/Buscar podcast/i);
+    fireEvent.change(searchImput, { target: { value: 'coding' } });
+    await waitFor(() => {
+        const podcastTitles = screen.getAllByText(/coding/i);
+        expect(podcastTitles.length).toBeGraterThan(0);
+    });
+});
+});
